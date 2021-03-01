2a3b65e05a9204baee4533b372796dd3c3dc292b
Phan An <me@phanan.net>
2021-03-01 19:31:38 +0100

    chore: cleaner type for Factoria

--- types.d.ts
+++ types.d.ts
@@ -1,20 +1,18 @@
 export declare namespace Factoria {
   type Attributes = Record<string, any>
-
   type Overrides<T> = Partial<{ [P in keyof T]: Overrides<T[P]> }> | ((faker: Faker.FakerStatic) => any)
-
   type StateDefinition = ((faker: Faker.FakerStatic) => Factoria.Attributes) | Attributes
 
-  type Factoria = (<T> (name: string, overrides?: Factoria.Overrides<T>) => T)
-    & (<T> (name: string, count: 1, overrides?: Factoria.Overrides<T>) => T)
-    & (<T> (name: string, count: number, overrides?: Factoria.Overrides<T>) => T[])
-    & {
-    define: <T> (
+  type Factoria = {
+    <T> (name: string, overrides?: Factoria.Overrides<T>): T
+    <T> (name: string, count: 1, overrides?: Factoria.Overrides<T>): T
+    <T> (name: string, count: number, overrides?: Factoria.Overrides<T>): T[]
+    define<T> (
       model: string,
       handler: (faker: Faker.FakerStatic) => Overrides<T>,
       states?: Record<string, StateDefinition>
-    ) => Factoria
-    states: (...states: string[]) => Factoria
+    ): Factoria
+    states (...states: string[]): Factoria
   }
 }
 
